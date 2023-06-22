b9cd706ea7e0327f098fa3c0541bd33b6da95571
maria_s_pr@mail.ru <maria_s_pr@mail.ru>
2023-06-22 20:12:50 +0500

    Task 25

---
+++ Ex04_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+//Возведение в степень числа
+int upow(int a, int b)
+{
+    int result = a;
+    for (int i = 1; i < b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+//Ввод чисел
+int[] ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+}
+//////////////////////////////////////////////////////////////////////
+int[] ab = ReadInt("Введите два числа через пробел:");
+System.Console.WriteLine($"{ab[0]} в степени {ab[1]} равно {upow(ab[0], ab[1])}");
